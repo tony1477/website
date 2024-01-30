2ea003f93a466fc93efc6c5e9421758c318efcd3
Martoni F <martonif1107@gmail.com>
2024-01-30 10:11:50 +0700

    Create Config.php
    Create direct from github and empty value

---
+++ app/core/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WPG\IT\Website\core;
+
+class Config
+{
+    const APP_NAMESPACE = 'WPG\\IT\\Website\\';
+    
+    const DB_HOST = '';
+    const DB_USER = '';
+    const DB_PWD = '';
+    const DB_NAME = '';
+    const SECRET_KEY = '';
+    const TOKEN_ALGO = 'HS256';
+    const MAX_TOKEN_LIMIT = 8*60*60;
+}
